e22ff2dff287a309e9fe67270cbadc8535a28319
Kartik <aggarwalkartik96@gmail.com>
2020-05-28 18:42:23 +0530

    Updates

---
+++ precache-manifest.326ed646e551e9865e3e530f526b69a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d42c00ab75b09d37f2a26cce90e35e",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "8698eb42fabfceaea2eb",
+    "url": "/hackernews/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1e6047f4adac14e54deb",
+    "url": "/hackernews/static/css/main.3f7da247.chunk.css"
+  },
+  {
+    "revision": "8698eb42fabfceaea2eb",
+    "url": "/hackernews/static/js/2.00f13875.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hackernews/static/js/2.00f13875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6047f4adac14e54deb",
+    "url": "/hackernews/static/js/main.2fcddfab.chunk.js"
+  },
+  {
+    "revision": "0b86008cd6d74afb3390",
+    "url": "/hackernews/static/js/runtime-main.eaab49d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackernews/static/media/logo.5d5d9eef.svg"
+  }
+]);
